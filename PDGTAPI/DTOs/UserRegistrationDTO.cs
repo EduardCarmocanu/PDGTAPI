f86068e57581e640c7f12d710c64e2727a294e06
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-04-26 15:00:01 +0200

    Feat: Implement Token base authentication

---
+++ PDGTAPI/DTOs/UserRegistrationDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDGTAPI.DTOs
+{
+	public class UserRegistrationDTO
+	{
+		[Required]
+		[StringLength(64, ErrorMessage = "First Name should be at least 2 characters and maximum 64 characters", MinimumLength = 2)]
+		public string FirstName { get; set; }
+		[Required]
+		[StringLength(64, ErrorMessage = "Last Name should be at least 2 characters and maximum 64 characters", MinimumLength = 2)]
+		public string LastName { get; set; }
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		public int RedCapRecordId { get; set; }
+		[RegularExpression(@"[abAB]")]
+		public char RedCapGroup { get; set; }
+	}
+}
--- PDGTAPI/Startup.cs
+++ PDGTAPI/Startup.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using PDGTAPI.Data;
+using PDGTAPI.Data.Entities;
+using PDGTAPI.Services;
 
 namespace PDGTAPI
 {
@@ -25,6 +33,7 @@ namespace PDGTAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddSingleton(Configuration);
 			services.AddCors(options =>
 			{
 				options.AddPolicy("DefaultPolicy", policy =>
@@ -35,11 +44,55 @@ namespace PDGTAPI
 				});
 			});
 
+			services.AddDbContext<ApplicationDataContext>(options =>
+			{
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"));
+			});
+				
+			services.AddIdentity<UserEntity, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDataContext>();
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.RequireHttpsMetadata = false;
+				options.SaveToken = true;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Security:JWT:Key"])),
+					ValidateAudience = true,
+					ValidAudience = Configuration["Security:JWT:Audience"],
+					ValidateIssuer = true,
+					ValidIssuer = Configuration["Security:JWT:Issuer"]
+				};
+			});
+
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy("Doctors", policy =>
+				{
+					policy.RequireRole("Doctor, Administrator");
+				});
+				options.AddPolicy("Patients", policy =>
+				{
+					policy.RequireRole("Patient");
+				});
+				options.AddPolicy("Administrators", policy =>
+				{
+					policy.RequireRole("Administrator");
+				});
+			});
+
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDataContext DataContext)
 		{
 			if (env.IsDevelopment())
 			{
@@ -50,6 +103,8 @@ namespace PDGTAPI
 				app.UseHsts();
 			}
 			app.UseCors("DefaultPolicy");
+			DataContext.Database.EnsureCreated();
+			app.UseAuthentication();
 			app.UseHttpsRedirection();
 			app.UseMvc();
 		}
