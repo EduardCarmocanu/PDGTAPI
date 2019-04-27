f22d14fdd8c0cc428524b38d97564903dc4f921b
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-04-27 02:55:34 +0200

    Feat: Create roles helper class

---
+++ PDGTAPI/Helpers/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDGTAPI.Helpers
+{
+	public static class Roles
+	{
+		public static string Administrator = "Administrator";
+		public static string Doctor = "Doctor";
+		public static string Patient = "Patient";
+	}
+}
