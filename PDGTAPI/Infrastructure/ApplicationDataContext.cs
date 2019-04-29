8332b60094fd8f65fea84611397db831cea40471
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-04-29 19:44:13 +0200

    Feat: move "UserEntity" to infrastructure folder

---
+++ PDGTAPI/Infrastructure/ApplicationDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PDGTAPI.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDGTAPI.Infrastructure
+{
+	public class ApplicationDataContext : IdentityDbContext<UserEntity, IdentityRole, string>
+	{
+		public ApplicationDataContext(DbContextOptions options) : base(options) { }
+	}
+}
---
+++ PDGTAPI/Infrastructure/DataSeeder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDGTAPI.Infrastructure
+{
+	public class DataSeeder
+	{
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public DataSeeder(RoleManager<IdentityRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+
+		public void Seed()
+		{
+			if (!_roleManager.Roles.Any())
+			{
+				_roleManager.CreateAsync(new IdentityRole("Administrator"));
+				_roleManager.CreateAsync(new IdentityRole("Doctor"));
+				_roleManager.CreateAsync(new IdentityRole("Patient"));
+			}
+		}
+	}
+}
