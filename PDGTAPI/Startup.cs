9e0cef5daa2edaa1ceca5f3d62d4684850fa0b43
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-04-29 19:52:46 +0200

    Feat: Use new Infrastructure namespace

--- PDGTAPI/Startup.cs
+++ PDGTAPI/Startup.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using PDGTAPI.Data;
-using PDGTAPI.Models.Entities;
+using PDGTAPI.Infrastructure;
+using PDGTAPI.Infrastructure.Entities;
 using PDGTAPI.Services;
 using RestSharp;
 
