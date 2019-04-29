126e893842ba406421a40534e356d45fc708c6c4
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-04-29 19:41:26 +0200

    Feat: Finish PostSessionQuestionnaire Models

---
+++ PDGTAPI/Models/Questionnaires/PostSessionQuestionnaire/PostSessionQuestionnaireBase.cs
@@ -0,0 +1,32 @@
+﻿using PDGTAPI.Models.Questionnaires.WeeklyQuestionnaire.Painkillers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDGTAPI.Models.Questionnaires.PostSessionQuestionnaire
+{
+	public abstract class PostSessionQuestionnaireBase
+	{
+		[Required]
+		[Range(0, 10)]
+		public int Pain { get; set; }
+
+		[Required]
+		public bool TakenPainkillers { get; set; }
+
+		public Painkiller[] Painkillers { get; set; }
+
+		[Required]
+		public bool TrainingSideEffects { get; set; }
+
+		[Required]
+		[StringLength(1024, ErrorMessage = "Comment is too long", MinimumLength = 16)]
+		public string TrainingSideEffectsDescription { get; set; }
+
+		[StringLength(1024, ErrorMessage = "Comment is tool long", MinimumLength = 16)]
+		public string Comments { get; set; }
+
+	}
+}
