f9c23390d09cc4fef56c953c8d9c560362c5267c
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-04-29 19:47:53 +0200

    Feat: Create WeeklyQuestionnaire

---
+++ PDGTAPI/Models/Questionnaires/WeeklyQuestionnaire/DueToTraining.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PDGTAPI.Models.Questionnaires;
+
+namespace PDGTAPI.Models.Questionnaires.WeeklyQuestionnaire
+{
+	public class DueToTraining
+	{
+		[Required]
+		public bool HasTaken { get; set; }
+		[Range(1, 7)]
+		public int DaysTaken { get; set; }
+		public Painkiller[] Painkillers { get; set; }
+	}
+}
---
+++ PDGTAPI/Models/Questionnaires/WeeklyQuestionnaire/SupplementaryTreatment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PDGTAPI.Models.Questionnaires.WeeklyQuestionnaire
+{
+	public class SupplementaryTreatment
+	{
+		[Required]
+		public bool SeenDoctorDueToShoulder { get; set; }
+		[Required]
+		public bool ReceivedTreatment { get; set; }
+		public string[] Treatments { get; set; }
+	}
+}
