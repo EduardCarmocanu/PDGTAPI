1c12dc276491e8f85e92dd3f03a751c1f95bf9be
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-05-01 21:56:45 +0200

    Feat: Implement patient baseline retrieval and unify all RedCapService methods

---
+++ PDGTAPI/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDGTAPI.Models
+{
+	public class UserInfo
+	{
+		public int RecordId { get; set; }
+		public DateTime BaselineDate { get; set; }
+		public char RandomisationGroup { get; set; }
+	}
+}
