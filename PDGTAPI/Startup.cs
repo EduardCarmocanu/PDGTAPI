ae7175ca12b774088cad8c896f1bba4f6da4eda1
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-05-01 15:49:37 +0200

    Feat: remove non-existend namespace

--- PDGTAPI/Startup.cs
+++ PDGTAPI/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using PDGTAPI.Helpers;
 using PDGTAPI.Infrastructure;
-using PDGTAPI.Infrastructure.Entities;
 using PDGTAPI.Services;
 using RestSharp;
 
