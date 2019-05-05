aef8445c3d66d2fca7a4abf781d2d7fe8fef7df3
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-05-05 18:17:19 +0200

    Feat: nullable guide image in DB

---
+++ PDGTAPI/Migrations/20190505105238_NUllableGuideImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PDGTAPI.Migrations
+{
+    public partial class NUllableGuideImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "GuideImage",
+                table: "Guide",
+                type: "image",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "image");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "GuideImage",
+                table: "Guide",
+                type: "image",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "image",
+                oldNullable: true);
+        }
+    }
+}
