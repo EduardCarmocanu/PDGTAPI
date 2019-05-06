e547211ebd1551f4062708e2fd72c755aaeb8d06
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-05-06 20:54:37 +0200

    Feat: remove generics

--- PDGTAPI/Models/Exercise.cs
+++ PDGTAPI/Models/Exercise.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace PDGTAPI.Models
 {
-	public class Exercise <TSet>
+	public class Exercise
 	{
 		[Required]
 		public bool IsSkipped { get; set; }
+		[StringLength(1024, ErrorMessage = "Comment is too long", MinimumLength = 16)]
 		public string SkippingReason { get; set; }
 		[Required]
-		public List<TSet> Sets { get; set; }
+		public Set Sets { get; set; }
 	}
 }
