c0917a3037c73627adb8867931538f996ffbf00e
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-05-09 10:04:33 +0200

    Feat: Add Time range identifier

---
+++ PDGTAPI/Migrations/20190507080753_TimeRangeRedCapIdentifier.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PDGTAPI.Migrations
+{
+    public partial class TimeRangeRedCapIdentifier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RedCapIdentifier",
+                table: "TimeRange",
+                maxLength: 2,
+                nullable: true);
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RedCapIdentifier",
+                table: "TimeRange");
+        }
+    }
+}
