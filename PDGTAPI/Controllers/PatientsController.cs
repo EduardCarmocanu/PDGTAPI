1ba46dd037da854c3158762f4879766d7bf0eac9
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-05-14 11:19:17 +0200

    Feat: clean patients controller

--- PDGTAPI/Controllers/PatientsController.cs
+++ PDGTAPI/Controllers/PatientsController.cs
@@ -36,37 +36,5 @@ namespace PDGTAPI.Controllers
 
 			return Ok();
 		}
-
-		// GET: api/Patients
-		[HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Patients/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Patients
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Patients/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
